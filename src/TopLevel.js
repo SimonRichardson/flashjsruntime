b93e95e93d9f33ef3e104c63c09dfe882cd8a70f
Simon Richardson <stickupkid@gmail.com>
2012-03-01 09:34:06 +0000

    Adding the reflect api

--- src/TopLevel.js
+++ src/TopLevel.js
@@ -1,19 +1,8 @@
 function trace() {
 	console.log(Array.prototype.slice.call(arguments).join(" "));
 }
+
+// Aliases
 function is(instance, klass) {
-	if(instance === klass || instance instanceof klass) {
-		return true;
-	} else {
-		var namespace = klass.reflection.namespace.toString();
-		var namespaces = instance.reflection.namespaces;
-		var total = namespaces.length;
-		for(var i=0; i<total; i++) {
-			trace(namespaces[i].toString());
-			if(namespaces[i].toString() === namespace) {
-				return true;
-			}
-		}
-	}
-	return false;
+	return utils.Reflect.is(instance, klass);
 }
---
+++ src/utils/Reflect.js
@@ -0,0 +1,19 @@
+var utils = utils || {};
+utils.Reflect = {
+	is: function(instance, klass) {
+		if(instance === klass || instance instanceof klass) {
+			return true;
+		} else {
+			var namespace = klass.reflection.namespace.toString();
+			var namespaces = instance.reflection.namespaces;
+			var total = namespaces.length;
+			for(var i=0; i<total; i++) {
+				trace(namespaces[i].toString());
+				if(namespaces[i].toString() === namespace) {
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+}
