eb318b41de57dac12b5804983011913352bd9037
Simon Richardson <stickupkid@gmail.com>
2012-03-04 22:35:00 +0000

    Starting to add different renderers

---
+++ src/render/RemedyWebGL.js
@@ -0,0 +1,33 @@
+var render = render || {};
+render.RemedyWebGL = RemedyBase.extend({
+	constructor: function(){
+		this.base();
+		
+		var scope = this;
+		
+		this._.isRunning = false;
+		
+		this.define('isRunning', {
+			get: function(){ return scope._.isRunning; }
+		});
+	},
+	start: function(){
+		this._.isRunning = true;
+	},
+	beginFill: function(color, alpha){
+	},
+	clear: function(){
+	},
+	drawRect: function(x, y, width, height){
+	},
+	drawCircle: function(x, y, radius){
+	},
+	drawEllipse: function(x, y, width, height){
+	},
+	lineTo: function(x, y){
+	},
+	moveTo: function(x, y){
+	},
+	endFill: function(){
+	}
+});
